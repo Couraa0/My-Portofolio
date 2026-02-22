b953f8e7f83fa6aaf613459b67abec567b3b547c
Couraa0 <muhammadrakhasyamputra@gmail.com>
2026-02-22 13:29:47 +0700

    Add Speed Insight Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import "@/lib/silence";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById("root")!).render(<App />);
