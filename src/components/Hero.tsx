ec3e7fe41fcc62589c0694f85b5f625f855cb554
Couraa0 <muhammadrakhasyamputra@gmail.com>
2026-03-19 05:07:35 +0700

    Fix CV Button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -103,15 +103,16 @@ const Hero = () => {
               {t("View My Projects")}
             </button>
             <div className="relative inline-flex items-center gap-4">
-              <a href="/Muhammad_Rakha_Syamputra_CV.pdf" download
+              <a href="https://drive.google.com/file/d/1JHdnHLOJfDU3Wf3jK1hrfgMrzbeGfQdT/view?usp=drive_link" 
+                target="_blank" rel="noopener noreferrer"
                 className="rounded-full border px-5 py-3 sm:px-7 sm:py-3.5 text-[13px] sm:text-sm font-semibold transition-all duration-300 hover:scale-105 border-border text-foreground hover:bg-secondary/80 flex items-center gap-2"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" x2="12" y1="15" y2="3"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline points="7 10 12 15 17 10" /><line x1="12" x2="12" y1="15" y2="3" /></svg>
                 {t("Resume")}
               </a>
               <a href="#contact"
                 className="rounded-full border px-5 py-3 sm:px-7 sm:py-3.5 text-[13px] sm:text-sm font-semibold transition-all duration-300 hover:scale-105 border-border text-foreground hover:border-primary hover:text-primary"
-                >
+              >
                 {t("Contact Me")}
               </a>
 
